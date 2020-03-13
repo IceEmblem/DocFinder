0a6513f207aa88a83b800ed1167f665022b99891
IceEmblem <1373611035@qq.com>
2020-03-12 19:11:15 -0700

    添加txt文本打开器

---
+++ src/DocFind/HeaderFiles/DocumentOpeners/TxtOpener.hpp
@@ -0,0 +1,22 @@
+#ifndef TxtOpener_H
+#define TxtOpener_H
+
+#include "../DocumentOpener.hpp"
+
+namespace DocFind {
+    class TxtOpener
+    {
+    private:
+        /* data */
+    public:
+        std::string getExecName() const{
+            static std::string execName = "Txt";
+            return execName;
+        }
+        bool isCanOpen(std::string docPostfix) const;
+        void open(std::string docPath, std::string execPath) const;
+    };
+    
+}
+
+#endif
---
+++ src/DocFind/SourceFiles/DocumentOpeners/TxtOpener.cpp
@@ -0,0 +1,21 @@
+#include <vector>
+#include "../../HeaderFiles/DocumentOpeners/TxtOpener.hpp"
+
+namespace DocFind {
+
+    bool TxtOpener::isCanOpen(std::string docPostfix) const{
+        static std::vector<std::string> postfixs = {"txt"};
+        for(auto posfix : postfixs){
+            if(docPostfix == posfix){
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    void TxtOpener::open(std::string docPath, std::string execPath) const{
+        std::string cmd = "\"" + execPath + "\"" + " " + "\"" + docPath + "\"";
+        system(cmd.c_str());
+    }
+}
